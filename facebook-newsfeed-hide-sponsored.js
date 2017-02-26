7b534e8cc090333e9a93a26100ce017d59c979e0
d-burke <omnidan@yahoo.com>
2017-02-25 21:14:17 -0800

    facebook newsfeed hide sponsored

---
+++ facebook-newsfeed-hide-sponsored.js
@@ -0,0 +1,12 @@
+//facebook newfeed hide 'sponsored'
+function adKill(w){
+  w = w || 'Sponsored';
+  var ns = document.body.getElementsByClassName('userContentWrapper');
+  for(var i=0;i<ns.length;i++){
+    if(ns[i].innerHTML.indexOf(w) > 0){
+      ns[i].style.display = 'none';
+      console.log('blocked this ad: ', ns[i]);
+    }
+  }
+};
+setInterval(adKill, 3000);
