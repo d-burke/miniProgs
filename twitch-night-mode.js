78bc2d19ff58012624a3fd7b3dd74edd57869791
d-burke <omnidan@yahoo.com>
2017-02-25 21:09:10 -0800

    twitch night mode

---
+++ twitch-night-mode.js
@@ -0,0 +1,5 @@
+//twitch night mode
+document.querySelector('.app-main').style.backgroundColor="#000000";
+document.querySelector('.cn-bar').style.backgroundColor="#000000";
+document.querySelector('.ember-chat').style.backgroundColor="#111111";
+document.querySelector('.ember-chat .chat-room').style.color="#999";
