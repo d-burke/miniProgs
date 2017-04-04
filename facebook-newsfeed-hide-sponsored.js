cfe32127528378524e1f82cf2586d381347f327e
d-burke <omnidan@yahoo.com>
2017-04-04 15:53:49 -0700

    facebook newsfeed hide sponsored 4/4/2017

--- facebook-newsfeed-hide-sponsored.js
+++ facebook-newsfeed-hide-sponsored.js
@@ -1,7 +1,7 @@
 //facebook newfeed hide 'sponsored'
 function adKill(w){
   w = w || 'Sponsored';
-  var ns = document.body.getElementsByClassName('userContentWrapper');
+  var ns = document.body.getElementsByClassName('fbUserContent');
   for(var i=0;i<ns.length;i++){
     if(ns[i].innerHTML.indexOf(w) > 0){
       ns[i].style.display = 'none';
