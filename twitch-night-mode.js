2103eccaeeb7ae9f0fa40034aa93b61213435a6d
d-burke <omnidan@yahoo.com>
2017-04-12 21:59:17 -0700

    twitch night mode 4/12/2017

--- twitch-night-mode.js
+++ twitch-night-mode.js
@@ -1,5 +1,80 @@
-//twitch night mode
+// twitch night mode last update April 12, 2017
+
+// live stream
+  // general background
+document.querySelector('.app-main').style.backgroundColor="#000000";
+  // top bar over vid
+document.querySelector('.cn-bar').style.backgroundColor="#000000";
+  // box below vid
+document.getElementsByClassName('brick')[0].style.backgroundColor="#000000";
+  // chat right bar
+document.querySelector('.ember-chat').style.backgroundColor="#111111";
+document.querySelector('.ember-chat .chat-room').style.color="#999";
+  // chat top bar
+document.querySelector('.chat-header').style.backgroundColor="#000000";
+  // cheer top bar
+document.querySelector('.pinned-cheer__top-bar').style.backgroundColor="#000000";
+
+// VOD
+  // general background
+document.querySelectorAll('.tse-content')[1].style.backgroundColor="#000";
+  // box below vid
+document.getElementsByClassName('brick')[0].style.backgroundColor="#000000";
+document.querySelectorAll('.brick')[1].style.backgroundColor="#000"
+  // top bar over vid
+document.querySelector('.cn-bar').style.backgroundColor="#000000";
+  // chat top bar
+document.querySelector('.top').style.backgroundColor="#000000";
+  // chat right bar
+document.querySelectorAll('.tse-content')[2].style.backgroundColor="#111";
+document.querySelectorAll('.tse-content')[2].style.color="#999";
+
+
+
+
+
+
+
+
+
+
+/////////
 document.querySelector('.app-main').style.backgroundColor="#000000";
 document.querySelector('.cn-bar').style.backgroundColor="#000000";
 document.querySelector('.ember-chat').style.backgroundColor="#111111";
 document.querySelector('.ember-chat .chat-room').style.color="#999";
+
+
+//twitch
+//night mode
+  // general background
+document.querySelector('.app-main').style.backgroundColor="#000000";
+  // top bar over vid
+document.querySelector('.cn-bar').style.backgroundColor="#000000";
+  // box below vid
+document.getElementsByClassName('brick')[0].style.backgroundColor="#000000";
+//chat right bar
+document.querySelector('.ember-chat').style.backgroundColor="#111111";
+document.querySelector('.ember-chat .chat-room').style.color="#999";
+  // chat top bar
+document.querySelector('.chat-header').style.backgroundColor="#000000";
+  // cheer top bar
+document.querySelector('.pinned-cheer__top-bar').style.backgroundColor="#000000";
+////-v
+document.getElementById('ember2185').style.backgroundColor="#000000";
+document.getElementById('ember2161').style.backgroundColor="#000000";
+document.querySelector('.chat-lines').style.backgroundColor="#111";
+//?
+//VOD
+//same as above v
+// box below vid
+document.getElementsByClassName('brick')[0].style.backgroundColor="#000000";
+  // top bar over vid
+document.querySelector('.cn-bar').style.backgroundColor="#000000";
+  // chat top bar
+document.querySelector('.top').style.backgroundColor="#000000";
+// ^
+// chat right bar
+document.querySelectorAll('.tse-content')[2].style.backgroundColor="#111";
+// general background
+document.querySelectorAll('.tse-content')[1].style.backgroundColor="#111";
