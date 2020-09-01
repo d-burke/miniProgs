97a8af1ef39eacf7faee42d5f651cf521dc93d5d
Dan Burke <omnidan@yahoo.com>
2020-09-01 00:09:51 -0700

    facebook filter newsfeed, last updated 9/1/2020
    facebook filter newsfeed, last updated 9/1/2020

--- facebook-filter-newsfeed.js
+++ facebook-filter-newsfeed.js
@@ -1,30 +1,27 @@
-//facebook hide newsfeed articles by word
-function newsFeedSnipe(w, t) {
-   w = w || 'Sponsored';
-   t = t || 3000;
-   var ns = document.body.getElementsByClassName('fbUserContent');
-   for (var i = 0; i < ns.length; i++) {
-      if (ns[i].innerHTML.indexOf(w) > 0) {
-         ns[i].parentNode.removeChild(ns[i]);
-         console.log('blocked an instance of ', w);
-      }
-   }
-   return setTimeout(function(){newsFeedSnipe(w)}, t);
+// facebook hide newsfeed articles containing target words
+// last updated 9/1/2020
+let targetWords = ['Sponsored', ' reacted to this.', ' replied to a ', 'was mentioned in a ', 'Friend Suggestions', ' commented on this.', ' is interested in an event.', ' liked this', 'Suggested Videos',  'Popular Live Video', ' was tagged in a photo.', '\'s cover photo.', 'Suggested for You'];
+
+function removeElement(e) {
+  if(!e){ return false;}
+  console.log('Removed element: ', e);
+  e.parentNode.removeChild(e);
+  return true;
 };
 
-//facebook newsfeed hide crap
-var filterNewsFeed = function(w) {
-   var rid = ['Sponsored', ' reacted to this.', ' replied to a ', 'was mentioned in a ', 'Friend Suggestions', ' commented on this.', ' is interested in an event.', ' liked this', 'Suggested Videos',  'Popular Live Video', ' was tagged in a photo.', '\'s cover photo.'];
-   if (w) { rid.push(w); }
-   setInterval(function() {
-      var rColumnChrome = document.getElementsByClassName('ego_section')[0];
-      if (rColumnChrome.style.display != 'none'){ rColumnChrome.style.display = 'none'; }
-      var rColumnFirefox = document.getElementById('rightCol');
-      if (rColumnFirefox.style.display != 'none'){ rColumnFirefox.style.display = 'none'; }
-   }, 3000);
-   for (var i = 0; i < rid.length; i++) {
-      newsFeedSnipe(rid[i]);
-   }
+function filterNewsFeed(t) {
+  t = t || 3000;
+  let ns = document.body.getElementsByClassName('sjgh65i0');
+  for (let i = 0; i < ns.length; i++) {
+    for (let j = 0; j < targetWords.length; j++) {
+      if (ns[i].innerHTML.indexOf(targetWords[j]) >= 0) {
+        if(removeElement(ns[i])) {
+          console.log('removed instace of: ', targetWords[j]);
+        }
+      }
+    }
+  }
+  return setTimeout(function(){filterNewsFeed()}, t);
 };
 
 filterNewsFeed();
